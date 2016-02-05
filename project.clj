4ba82123d50d8b01e4d5a3eea7cc918e4f7bd687
Trevor Hartman <trevorhartman@gmail.com>
2016-02-04 21:53:43 -0700

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot-codeclimate "0.1.6-SNAPSHOT"
+(defproject yetibot-codeclimate "0.1.6"
   :description "Integration with CodeClimate"
   :url "https://github.com/devth/yetibot-codeclimate"
   :lein-release {:deploy-via :clojars}
